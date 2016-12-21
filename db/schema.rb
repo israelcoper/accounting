82bfd4833048425223bfcdfe01b55254aad851f2
Israel B. Coper <coper_israelb@yahoo.com>
2016-12-21 14:37:39 +0800

    Add description to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221031959) do
+ActiveRecord::Schema.define(version: 20161221063010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20161221031959) do
     t.integer  "account_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
